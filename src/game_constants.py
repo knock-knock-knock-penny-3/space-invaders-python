84f2451e1211018fb0e7063701bc8ddc21b0c911
Enrico Sanna <antani@tiscali.it>
2023-02-14 16:23:47 +0100

    adding world constants

--- src/game_constants.py
+++ src/game_constants.py
@@ -1,7 +1,18 @@
 """
 """
 
-MAIN_BG_COLOR = (30, 30, 30)  # #1E1E1E
-STARSHIP_COLOR = 'orange'
+from pygame.math import Vector2
+
+ALIEN_COLOR = [
+    (255, 0, 255),              # #FF00FF
+    (0, 253, 255),              # #00FDFF
+    (0, 254, 255),              # #00FEFF
+    (0, 255, 0),                # #00FF00
+    (0, 254, 0)                 # #00FE00
+]
+EMPTY_PIXEL = ' '
+MAIN_BG_COLOR = (30, 30, 30)    # #1E1E1E
+PIXEL_SIZE = Vector2(4, 4)
+STARSHIP_COLOR = (0, 254, 254)  # #00FEFE
 START_STARSHIPS = 3
 WORLD_CONSTRAINTS = {}
