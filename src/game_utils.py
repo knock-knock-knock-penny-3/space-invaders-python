f0ae449175057bf36309e7aacfaacdf2280da821
Enrico Sanna <antani@tiscali.it>
2023-02-14 17:13:59 +0100

    adding generic function to load sounds and adding heartbeat sounds

--- src/game_utils.py
+++ src/game_utils.py
@@ -1,8 +1,11 @@
+import os, sys
+
 from pygame import draw, Rect, SRCALPHA
 from pygame.math import Vector2
+from pygame.mixer import Sound
 from pygame.surface import Surface
 
-from game_constants import EMPTY_PIXEL, PIXEL_SIZE
+from game_constants import EMPTY_PIXEL, PIXEL_SIZE, RESOURCES_DIR_NAME, SOUNDS_DIR_NAME
 
 def draw_sprite(schema, color):
     width = max([len(row) for row in schema])
@@ -17,3 +20,15 @@ def draw_sprite(schema, color):
                 draw.rect(sprite, color, rect)
 
     return sprite
+
+def load_sound(filename):
+    """docstring"""
+    path = os.path.join(RESOURCES_DIR_NAME, SOUNDS_DIR_NAME, filename)
+    return Sound(_resource_path(path))
+
+def _resource_path(relative_path):
+    """docstring"""
+    if hasattr(sys, "_MEIPASS"):
+        base = getattr(sys, "_MEIPASS")
+        return os.path.join(base, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
